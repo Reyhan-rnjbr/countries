733ea622209c25f26fa0dada7b41c209b74e1c64
reyhaneranjbar <reyhane.ranjbar.93@gmail.com>
2026-02-04 11:07:19 +0330

    df

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const countries = document.getElementById("countries");
-fetch("https://restcountries.com/v3.1/all?fields=name,capital,flags,population,region,cca3").then((res)=>{
+const dark = document.getElementById("btn-dark");
+const light = document.getElementById("btn-light");
 
-    return res.json()
+dark.addEventListener("click", () => {
+  document.documentElement.classList.add("dark");
+  console.log("object");
+});
 
-}).then((data)=>{
-    data.forEach((value)=>{
-         countries.innerHTML+= `<script src="./main.js"></script>
+light.addEventListener("click", () => {
+  document.documentElement.classList.remove("dark");
+  console.log("Hoo");
+});
+
+fetch(
+  "https://restcountries.com/v3.1/all?fields=name,capital,flags,population,region,cca3"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((value) => {
+      countries.innerHTML += `<script src="./main.js"></script>
     <li>
         <div> <img src="${value.flags.png}" alt=""></div>
         <div>
@@ -14,7 +29,6 @@ fetch("https://restcountries.com/v3.1/all?fields=name,capital,flags,population,r
             <p>Region: ${value.region}</p>
             <p>Capital: ${value.capital[0]}</p>
         </div>
-    </li>`
-    })
-   
-})
+    </li>`;
+    });
+  });
